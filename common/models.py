c2c800576a5c7124238dcb7c239a5d6d261559a8
PrincesaTyne <justinejjingo@gmail.com>
2024-03-29 18:03:11 +0300

    add base and auditable models

---
+++ common/models.py
@@ -0,0 +1,24 @@
+import shortuuid
+from django.db import models
+
+
+class AuditableModel(models.Model):
+    ''' Auditable base model '''
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    created_by = models.CharField(max_length=256, null=True, blank=True)
+    updated_by = models.CharField(max_length=256, null=True, blank=True)
+    deleted_by = models.CharField(max_length=256, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModel(AuditableModel):
+    '''Base model'''
+    id = models.CharField(max_length=20, primary_key=True, default=shortuuid.uuid)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
