1f5e5fd19169376bd1d8a937ae0cfedf91dfab43
PrincesaTyne <justinejjingo@gmail.com>
2024-03-29 18:21:10 +0300

    add accounts app & User model

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from common.models import BaseModel
+
+
+class User(BaseModel):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=256, null=True, blank=True)
+    username = models.CharField(max_length=100, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
